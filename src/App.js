44e3587043eb62006c9209120687278a5c7cc54a
fhonoria <83973848+fhonoria@users.noreply.github.com>
2021-08-19 21:25:55 +0200

    added the Github repo URL

--- src/App.js
+++ src/App.js
@@ -2,5 +2,22 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a href="https://optimistic-hopper-ee95fa.netlify.app" target="_blank">
+          Honoria Fraszt
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/fhonoria/weather-search-app"
+          target="_blank"
+        >
+          open-sourced on Git-hub
+        </a>
+      </footer>
+    </div>
+  );
 }
