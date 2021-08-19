df2a0d4ddcbb04e93b654d06f90e88fcfb394f9f
fhonoria <83973848+fhonoria@users.noreply.github.com>
2021-08-19 21:30:36 +0200

    added rel to links

--- src/App.js
+++ src/App.js
@@ -7,13 +7,18 @@ export default function App() {
       <h1>Weather App</h1>
       <footer>
         This project was coded by{" "}
-        <a href="https://optimistic-hopper-ee95fa.netlify.app" target="_blank">
+        <a
+          href="https://optimistic-hopper-ee95fa.netlify.app"
+          target="_blank"
+          rel="noreferrer"
+        >
           Honoria Fraszt
         </a>{" "}
         and is{" "}
         <a
           href="https://github.com/fhonoria/weather-search-app"
           target="_blank"
+          rel="noreferrer"
         >
           open-sourced on Git-hub
         </a>
