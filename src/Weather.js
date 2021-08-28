c9d9587b45a709621c0a37451930960183fb6bc1
fhonoria <83973848+fhonoria@users.noreply.github.com>
2021-08-28 14:00:40 +0200

    animated weather icon

--- src/Weather.js
+++ src/Weather.js
@@ -19,6 +19,7 @@ export default function Weather(props) {
       wind: response.data.wind.speed,
       city: response.data.name,
       date: new Date(response.data.dt * 1000),
+      icon: response.data.weather[0].icon,
       iconUrl: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
     });
   }
--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,5 +1,6 @@
 import React from "react";
 import FormatDate from "./FormatDate";
+import WeatherIcon from "./WeatherIcon";
 
 export default function WeatherInfo(props) {
   return (
@@ -13,9 +14,11 @@ export default function WeatherInfo(props) {
       </ul>
       <div className="row mb-3">
         <div className="col-6">
-          <img src={props.data.iconUrl} alt="Sunny icon"></img>
-          <span className="temperature">{props.data.temperature}</span>
-          <span className="unit">°C</span>
+          <div className="float-left">
+            <WeatherIcon code={props.data.icon} />
+            <span className="temperature ms-2">{props.data.temperature}</span>
+            <span className="unit">°C</span>
+          </div>
         </div>
         <div className="col-6">
           <ul>
